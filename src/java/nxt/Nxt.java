21d683c73aecbd37e33f3ce320656e814ff584c5
Steven <steven@sigwo.com>
2018-01-25 10:55:44 -0500

    [Update] Jupiter Version

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -50,7 +50,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.11.4.1";
+    public static final String VERSION = "1.11.5.1";
     public static final String APPLICATION = "JRS";
 
     private static volatile Time time = new Time.EpochTime();

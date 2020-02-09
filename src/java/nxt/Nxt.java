4b29fb4982a8f095928199ac9108b232fddb6594
Steven <steven@sigwo.com>
2020-02-08 19:54:52 -0600

    1.13.0 version

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -50,7 +50,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.12.2e";
+    public static final String VERSION = "1.13.0";
     public static final String APPLICATION = "JRS";
 
     private static volatile Time time = new Time.EpochTime();

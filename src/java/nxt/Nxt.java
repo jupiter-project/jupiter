58e4b2e81eed2d1ed03d82cd906999f7310c1072
Javier Pavón <javierpavon2000@gmail.com>
2021-05-08 20:48:31 +0200

    v.1.4.0

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -50,7 +50,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.13.2";
+    public static final String VERSION = "1.14.0";
     public static final String APPLICATION = "JRS";
 
     private static volatile Time time = new Time.EpochTime();

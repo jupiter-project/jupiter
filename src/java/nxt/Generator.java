57be8821098476f075aea8c1ef84d7d8067728cf
Javier Pavón <javierpavon2000@gmail.com>
2021-06-04 02:24:02 +0200

    fix encoding

--- src/java/nxt/Generator.java
+++ src/java/nxt/Generator.java
@@ -1,6 +1,6 @@
 /*
- * Copyright © 2013-2016 The Nxt Core Developers.
- * Copyright © 2016-2017 Jelurida IP B.V.
+ * Copyright Â© 2013-2016 The Nxt Core Developers.
+ * Copyright Â© 2016-2017 Jelurida IP B.V.
  *
  * See the LICENSE.txt file at the top-level directory of this distribution
  * for licensing information.

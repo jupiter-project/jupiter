9eb53fe73229bd609570d50a66241f104012cec7
Javier Pavón <javierpavon2000@gmail.com>
2021-06-13 01:14:00 +0200

    increase default max base target

--- src/java/nxt/BlockImpl.java
+++ src/java/nxt/BlockImpl.java
@@ -483,11 +483,7 @@ final class BlockImpl implements Block {
                 }
             }
             
-            Logger.logDebugMessage(String.valueOf(getMaxBaseTarget(previousBlock)));
-            Logger.logDebugMessage(String.valueOf(Constants.ORIGINAL_MAX_BASE_TARGET));
-            Logger.logDebugMessage(String.valueOf(Constants.MAX_BASE_TARGET));
             if (baseTarget < 0 || baseTarget > getMaxBaseTarget(previousBlock)) {
-            	Logger.logDebugMessage("BASE TARGET MAX VALUE");
                 baseTarget = getMaxBaseTarget(previousBlock);
             }
             if (baseTarget < Constants.MIN_BASE_TARGET) {

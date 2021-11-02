ca7eb56ee3925a383a1ebff7b1f7c7566e6a75ec
Steven <steven@sigwo.com>
2021-11-02 08:43:37 -0400

    Added start script for service providers.
    Use this script to bump JVM memory allowances and refrain from heap errors under heavy account loads. This happens when old, very active accounts are opened.

---
+++ service_start.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Use this start script if you are providing a Jupiter node as a service
+
+APPLICATION="jrs"
+if [ -e ~/.${APPLICATION}/nxt.pid ]; then
+    PID=`cat ~/.${APPLICATION}/nxt.pid`
+    ps -p $PID > /dev/null
+    STATUS=$?
+    if [ $STATUS -eq 0 ]; then
+        echo "Jupiter server already running"
+        exit 1
+    fi
+fi
+mkdir -p ~/.${APPLICATION}/
+DIR=`dirname "$0"`
+cd "${DIR}"
+if [ -x jre/bin/java ]; then
+    JAVA=./jre/bin/java
+else
+    JAVA=java
+fi
+# Adapt -Xms and -Xmx to your needs
+nohup ${JAVA} -Xms4096m -Xmx8192m -cp classes:lib/*:conf:addons/classes:addons/lib/* -Dnxt.runtime.mode=desktop nxt.Nxt > /dev/null 2>&1 &
+echo $! > ~/.${APPLICATION}/nxt.pid
+cd - > /dev/null
